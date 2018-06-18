843fbbbba66d409ceae65b588927bf8d7f8d192a
Israel Dago <dagoisrael@gmail.com>
2018-06-18 22:14:36 +0300

    refactoring 01

--- src/main/java/db/migration/V0_0_3__select_test.java
+++ src/main/java/db/migration/V0_0_3__select_test.java
@@ -4,17 +4,11 @@ import com.israeldago.flywayDemo.entities.Car;
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.List;
-
 public class V0_0_3__select_test implements SpringJdbcMigration {
     @Override
-    public void migrate(JdbcTemplate jdbcTemplate) /*throws Exception */{
+    public void migrate(JdbcTemplate jdbcTemplate) {
         jdbcTemplate.query("select * from cars",
                 (rs, i) -> new Car(rs.getInt("id"), rs.getString("brand"), rs.getString("color")))
-                .stream()
-                //.peek(car -> System.out.println("Got " + car))
                 .forEach(System.out::println);
-
-
     }
 }
